e2f8a3eae37e9c1532be25ed5537b96a63ec2d0c
Smyllet <smyllet.gamer@gmail.com>
2020-11-11 18:21:01 +0100

    feat : Connexion à Discord
    + connexion à discord
    + activité du bot
    + récupération des erreurs
    
    CONFIG CHANGE : token et activity au fichier config

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-// Node Modules
+// - - - Node Modules - - - //
+const Discord = require('discord.js')
 
-
-// Chargement Config
+// - - - Chargement Config - - - //
 const config = require('./config.json')
 
+// - - - Instantiation du bot - - - //
+const dBot = new Discord.Client()
 
+// - - - Connexion à Discord - - - //
+dBot.login(config.bot.discord.token).catch(console.error)
 
-//todo
+// - - - Discord Bot Event - - - //
+// Au démarrage du bot
+dBot.on('ready', () => {
+    dBot.user.setPresence({ activity : {name : config.bot.discord.activity}, status : "online"})
+    console.log('Bot discord en ligne')
+})
+
+dBot.on('error', console.error)
